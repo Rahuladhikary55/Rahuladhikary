2af64f805efaf5a4102082a8d76299d02f1a1ea0
Rahuladhikary55 <122194624+Rahuladhikary55@users.noreply.github.com>
2023-01-10 22:13:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Rahulofficial5/Rahuladhikary.git /Rahuladhikary
+git clone https://github.com/Rahuladhikary55/Rahuladhikary.git /Rahuladhikary
 cd /Rahuladhikary
 pip3 install -r requirements.txt
 echo "Starting Bot...."
